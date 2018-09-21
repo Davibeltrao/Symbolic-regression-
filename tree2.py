ec7c87048cf31db1cca860307b9faad10893b192
davi <davibeltrao02@gmail.com>
2018-09-21 00:57:19 -0300

    Crossover 'working'. Mutation 'working'. Need parameter fixes

--- tree2.py
+++ tree2.py
@@ -5,6 +5,7 @@ import csv
 import numpy as np
 from sklearn.metrics import mean_squared_error
 from math import sqrt
+import copy
 
 
 gen_size = 50
@@ -50,19 +51,9 @@ class Tree(Node):
 		return False
 
 
-	def __str__(self, level=0):
-		ret = "    "*level+repr(self.name)+"\n"
-		for child in self.children:
-			ret += child.__str__(level+1)
-		return ret
-
-	def __repr__(self):
-		return '<tree node representation>'
-
-
-def get_node_type(terminal_only=False):
-	node_type = randint(0, 5)
-	if node_type == 0 or terminal_only==True:
+def get_node_type(terminal_only=False, first=False):
+	node_type = randint(0, 15)
+	if node_type == 0 or terminal_only==True and not first:
 		terminal_node = randint(0, len(terminals)-1)
 		return terminals[terminal_node]
 	else:
@@ -70,9 +61,9 @@ def get_node_type(terminal_only=False):
 		return functions[function_node]	
 
 
-def create_initial_pop(max_depth=2, node=None):
+def create_initial_pop(max_depth=3, node=None):
 	if node == None:
-		node_type = get_node_type()
+		node_type = get_node_type(first=True)
 		if(node_type == 'rand'):
 			node_type = randint(-5, 5)
 		#create root node
@@ -84,15 +75,15 @@ def create_initial_pop(max_depth=2, node=None):
 		elif node_type in ['sin', 'cos', 'exp']:
 			root.arity = 1
 			#criar um filho recursivamente
-			create_initial_pop(node=root)
+			create_initial_pop(max_depth, node=root)
 		else:
 			root.arity = 2
 		
 			#criar dois filhos recursivamente
-			create_initial_pop(node=root)
-			create_initial_pop(node=root)
+			create_initial_pop(max_depth, node=root)
+			create_initial_pop(max_depth, node=root)
 		
-		pop_list.append(root)
+		#pop_list.append(root)
 		return root
 	
 	elif node.depth == max_depth or node.arity == 0:
@@ -132,12 +123,12 @@ def create_initial_pop(max_depth=2, node=None):
 		elif node_type in ['sin', 'cos', 'exp']:
 			leaf.arity = 1
 			#criar somente um filho(arity 1)
-			create_initial_pop(node=leaf)
+			create_initial_pop(max_depth, node=leaf)
 		else:
 			leaf.arity = 2
 			#chamar recursivamente para criar dois nós filhos(arity 2)
-			create_initial_pop(node=leaf)
-			create_initial_pop(node=leaf)
+			create_initial_pop(max_depth, node=leaf)
+			create_initial_pop(max_depth, node=leaf)
 
 		#print(leaf)
 		return
@@ -171,6 +162,8 @@ def calculate_fitness(tree, x, y):
 	for i in range(0, len(y)):
 		ind_fitness += (y[i] - calculate_tree_value(tree, x[i])) * (y[i] - calculate_tree_value(tree, x[i]) )
 
+	ind_fitness = ind_fitness/len(y)
+
 	ind_fitness = sqrt(ind_fitness)
 
 	print(ind_fitness)
@@ -190,56 +183,103 @@ def tournament_selection(pop, k=2):
 
 	print("Fit: ", fitness)
 	print(best)
-	return best
+	return best, fitness
 
+
+def split_tree(parent, cut_height=None):
+	tree_1 = None
+	tree_2 = None
+
+	for pre, fill, node in RenderTree(parent):
+		if node.depth == 1 and tree_1 == None:
+			tree_1 = node
+			node.parent = None
+		
+
+	for pre, fill, node in RenderTree(parent):
+		if node.depth == 1 and tree_2 == None and tree_1 != None:
+			tree_2 = node
+			node.parent = None
+
+	return tree_1, tree_2
 
 def crossover(parent_1, parent_2):
 	print(">>>>>>>>>>>CROSSOVER<<<<<<<<<<<<")
 	print("\n\n")
 
 	print(parent_1)
-
 	print(parent_2)
+
+	tree_test = parent_1
+	tree_test2 = parent_2
 
 
 	crossover_1_height = int(parent_1.height/3)
 	crossover_2_height = int(parent_2.height/3)
-	#print("Cross Height: ", parent_1.height)
+	print("Cross Height: ", parent_1.height)
+	print("Cross Height: ", parent_2.height)
+
+	if parent_2.height <= 1 or parent_1.height <= 1:
+		print("Returning crossover. Small individual size")
+		return parent_1
 
 	tree_1 = None
 	tree_2 = None
 
-	for pre, fill, node in RenderTree(parent_1):
+	tree_3 = None
+	tree_4 = None
+
+
+	print("Parent 1 oi")
+	for pre, fill, node in RenderTree(tree_test):
 		print(pre, node.name)
 
-	for pre, fill, node in RenderTree(parent_2):
+	print('Parent 2 oi')
+	for pre, fill, node in RenderTree(tree_test2):
 		print(pre, node.name)
 
-		
-	print("Parent")
-	print(parent_1)
+	if tree_test.arity == 2:	
+		tree_1, tree_2 = split_tree(tree_test)
 
-	for pre, fill, node in RenderTree(parent_1):
-		for child in node.children:
-			print("Name: ", child.name)
-			#node = parent_2
-			#parent_2.parent = node.parent
-			break
+	if tree_test2.arity == 2:
+		tree_3, tree_4 = split_tree(tree_test2)
 	
-	print("New parent")
-	print(parent_1)
 
-	# for pre, fill, node in RenderTree(parent_2):
-	# 	if(node.height == crossover_2_height):
-	# 		tree_2 = node
-	# 		break
+	for pre, fill, node in RenderTree(tree_test):
+		if node.depth == 0 and tree_1 != None and tree_3 != None: 
+			tree_3.parent = node
+			tree_1.parent = node
 
-	# print("Tree_1: ", tree_1)
-	# for pre, fill, node in RenderTree(tree_1):
-	# 	print(pre, node.name)
-	# print("Tree_2: ", tree_2)
-	# for pre, fill, node in RenderTree(tree_2):
-	# 	print(pre, node.name)
+	print("Parent 1")
+	for pre, fill, node in RenderTree(tree_test):
+		print(pre, node.name)
+
+	return tree_test
+
+def mutation(tree):
+	subtree = create_initial_pop(max_depth=1)
+
+	print('SubTree')
+	for pre, fill, node in RenderTree(subtree):
+		print(pre, node.name)
+
+	print('Tree')
+	for pre, fill, node in RenderTree(tree):
+		print(pre, node.name)
+
+	for pre, fill, node in RenderTree(tree):
+		if node.depth == 1:
+			node.parent = None
+			break
+
+	for pre, fill, node in RenderTree(tree):
+		if node.depth == 1:
+			subtree.parent = tree
+
+	print('Tree')
+	for pre, fill, node in RenderTree(tree):
+		print(pre, node.name)	
+
 	
 def printtree(tree_node):
     if tree_node.left is not None:
@@ -252,11 +292,16 @@ def printtree(tree_node):
 fileLoad = FileLoad(dataset_path)
 x, y = fileLoad.getData()
 
-for i in range(2000):
+for i in range(10):
 	tree = create_initial_pop()
 
 	#printtree(tree)
 	#print(tree)
+
+
+	print("Tree")
+	for pre, fill, node in RenderTree(tree):
+		print(pre, node.name)
 
 	#tree_value = calculate_tree_value(tree, x)
 	#print("Value: ", tree_value)
@@ -271,14 +316,28 @@ for i in range(2000):
 
 
 #Calculate children population
+
 child_population = []
 while len(child_population) < gen_size:
-	best_1 = tournament_selection(pop_fitness, k=200)
-	best_2 = tournament_selection(pop_fitness, k=200)
+	best_1, fit_1 = tournament_selection(pop_fitness, k=2)
+	best_2, fit_2 = tournament_selection(pop_fitness, k=2)
 
-	#crossover(best_1, best_2)
+	best_test = copy.deepcopy(best_1)
+	best_test2 = copy.deepcopy(best_2)
 
+	#new_ind = crossover(best_test, best_test2)
+
+	mutation(best_test)
 	break
+	# fitness = calculate_fitness(new_ind, x, y)
+
+	# print("Fit1: ", fit_1)
+	# print("Fit2: ", fit_2)
+	# print("New ind_fitness: ", fitness)	
+
+	# child_population.append(new_ind)
+
+	#break
 
 
 
