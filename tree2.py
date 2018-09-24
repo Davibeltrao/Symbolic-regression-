8bb78ef24eb1332dac60e5f7c43d750abe4362a9
davi <davibeltrao02@gmail.com>
2018-09-24 18:20:18 -0300

    SR working. Mutation need fix

--- tree2.py
+++ tree2.py
@@ -5,10 +5,12 @@ import csv
 import numpy as np
 from sklearn.metrics import mean_squared_error
 from math import sqrt
+import math
 import copy
 
 
 gen_size = 150
+#terminals = ['x0', 'x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7']
 terminals = ['x0', 'x1']
 functions = ['sum', 'sub', 'div', 'mul']
 
@@ -17,7 +19,7 @@ pop_fitness = []
 
 dataset_path = 'datasets/synth1/synth1-train.csv'
 
-max_depth = 3
+max_depth = 4
 
 tree_id = 0
 
@@ -65,7 +67,7 @@ def get_node_type(terminal_only=False, first=False):
 		return functions[function_node]	
 
 
-def create_initial_pop(max_depth=3, node=None):
+def create_initial_pop(max_depth=4, node=None):
 	global tree_id
 	if node == None:
 		node_type = get_node_type(first=True)
@@ -78,7 +80,7 @@ def create_initial_pop(max_depth=3, node=None):
 		#get arity for root oeprator
 		if node_type in terminals:
 			root.arity = 0
-		elif node_type in ['sin', 'cos', 'exp']:
+		elif node_type in ['sin', 'cos', 'exp', 'log']:
 			root.arity = 1
 			#criar um filho recursivamente
 			create_initial_pop(max_depth, node=root)
@@ -130,7 +132,7 @@ def create_initial_pop(max_depth=3, node=None):
 		#get arity for leaf operator
 		if node_type in terminals:
 			leaf.arity = 0
-		elif node_type in ['sin', 'cos', 'exp']:
+		elif node_type in ['sin', 'cos', 'exp', 'log']:
 			leaf.arity = 1
 			#criar somente um filho(arity 1)
 			create_initial_pop(max_depth, node=leaf)
@@ -156,12 +158,30 @@ def calculate_tree_value(tree, x):
 		if(dividendo >= -0.05 and dividendo <= 0.05):
 			dividendo = 1
 		return calculate_tree_value(tree.children[0], x) / dividendo
+	if(tree.name =='log'):
+		log = calculate_tree_value(tree.children[0], x)
+		if log <= 1:
+			log = 2 
+		return math.log(log, 2)
+	
 	if(tree.name == 'x0'):
 		#retornar valor de x0
 		return x[0]
 	if(tree.name == 'x1'):
 		#retornar valor x1
 		return x[1]
+	if(tree.name == 'x2'):
+		return x[2]
+	if(tree.name == 'x3'):
+		return x[3]
+	if(tree.name == 'x4'):
+		return x[4]
+	if(tree.name == 'x5'):
+		return x[5]
+	if(tree.name == 'x6'):
+		return x[6]
+	if(tree.name == 'x7'):
+		return x[7]
 	if(tree.name in terminals):
 		return int(tree.name)
 	pass
@@ -176,6 +196,7 @@ def calculate_fitness(tree, x, y):
 
 	ind_fitness = sqrt(ind_fitness)
 
+	
 	#print(ind_fitness)
 
 	#fitness = y - tree_value
@@ -200,6 +221,7 @@ def split_tree(parent, cut_height=None):
 	tree_list = []
 	tree_1 = None
 	tree_2 = None
+
 	cut_depth = parent.height-1
 
 	#print("Height Parent: ", parent.height)
@@ -279,11 +301,11 @@ def crossover(parent_1, parent_2):
 	# for pre, fill, node in RenderTree(tree_test2):
 	# 	print(pre, node.name)
 
-	if tree_test.arity == 2:	
-		tree_1, tree_2, cut_depth_1 = split_tree(tree_test)
+	#if tree_test.arity == 2:	
+	tree_1, tree_2, cut_depth_1 = split_tree(tree_test)
 
-	if tree_test2.arity == 2:
-		tree_3, tree_4, cut_depth_2 = split_tree(tree_test2)
+#	if tree_test2.arity == 2:
+	tree_3, tree_4, cut_depth_2 = split_tree(tree_test2)
 	
 	# print("Arvore a ser substituida")
 	# for pre, fill, node in RenderTree(tree_test):
@@ -316,17 +338,17 @@ def mutation(tree):
 	# 	print(pre, node.name)
 
 	for pre, fill, node in RenderTree(tree):
-		if node.depth == 1:
+		if node.depth == tree.height-3-1:
 			node.parent = None
 			break
 
 	for pre, fill, node in RenderTree(tree):
-		if node.depth == 1:
+		if len(node.children) < node.arity:
 			subtree.parent = tree
 
-	# print('Tree')
-	# for pre, fill, node in RenderTree(tree):
-	# 	print(pre, node.name)	
+	# # print('Tree')
+	# # for pre, fill, node in RenderTree(tree):
+	# # 	print(pre, node.name)	
 
 	return tree
 
@@ -374,6 +396,16 @@ while 1:
 	while len(child_population) < gen_size:
 		best_1, fit_1 = tournament_selection(pop_fitness, k=3)
 		best_2, fit_2 = tournament_selection(pop_fitness, k=3)
+		different_ind = False
+		cont = 0
+		
+		"""
+		while different_ind == False:
+			best_2, fit_2 = tournament_selection(pop_fitness, k=3)
+			cont += 1
+			if best_2.tree_id != best_1.tree_id or cont > 15:
+				different_ind = True
+		"""
 
 		first_ind = copy.deepcopy(best_1)
 		second_ind = copy.deepcopy(best_2)
@@ -386,20 +418,29 @@ while 1:
 			new_ind = crossover(first_ind, second_ind)		
 
 		mutation_chance = randint(0, 100)
-		if mutation_chance <= 10:
+		if mutation_chance <= 20:
 			#print("Entrei mutation")
 			new_ind = mutation(new_ind)
-			#pass
+			pass
 		
+		new_ind.tree_id += 1
+
 		fitness = calculate_fitness(new_ind, x, y)
+
+		if fit_1 < fitness:
+			child_population.append( (best_1, fit_1) )
+			fitness_list.append(fit_1)
+			#pass
+		else:
+			child_population.append( (new_ind, fitness) )
+			fitness_list.append(fitness)
+	
 
 		# print("Fit1: ", fit_1)
 		# print("Fit2: ", fit_2)
 		# print("New ind_fitness: ", fitness)	
 
-		child_population.append( (new_ind, fitness) )
-		fitness_list.append(fitness)
-
+		
 	worst_fitness = np.amax(fitness_list)
 	best_fitness = np.amin(fitness_list)
 	average_fitness = np.average(fitness_list)
@@ -407,7 +448,7 @@ while 1:
 	print("Best: ", best_fitness, " Worst: ", worst_fitness, " Average: ", average_fitness)
 	#for ind, fit in child_population:
 
-
+	pop_fitness = []
 	pop_fitness = copy.deepcopy(child_population)
 
 
